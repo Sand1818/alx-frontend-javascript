48f12631ad218ac62c88548f96702bc9e42b9632
Sand1818 <sandiso.jele@gmail.com>
2024-08-08 15:04:43 +0200

    Adding readme and 0-5

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,32 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._currency = currency;
+    this._amount = amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set currency(Currency) {
+    this._currency = Currency;
+  }
+
+  set amount(Amount) {
+    this._amount = Amount;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this.currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
